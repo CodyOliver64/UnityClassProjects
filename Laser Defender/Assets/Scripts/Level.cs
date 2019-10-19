43444588cad3c38acf5050d02a227646d08c26c8
CodyOliver64 <56767738+CodyOliver64@users.noreply.github.com>
2019-10-19 13:19:53 -0400

    Add Laser Defender

---
+++ Laser Defender/Assets/Scripts/Level.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] float delayInSeconds = 2f;
+
+    public void LoadStartMenu()
+    {
+        //be careful hard coding numbers for scene (ok here since start will always be 1st)
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        //we can also load by the name of the scene
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("Game Over");
+    }
+
+}
