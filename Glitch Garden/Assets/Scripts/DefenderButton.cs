e6f2eede1fee26127defc47b7cc93f61170f9897
CodyOliver64 <56767738+CodyOliver64@users.noreply.github.com>
2019-10-30 16:09:34 -0400

    Add Glitch Garden

---
+++ Glitch Garden/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefenderButton : MonoBehaviour
+{
+
+    [SerializeField] Defender defenderPrefab;
+
+    private void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+
+        if (!costText)
+        {
+            //Debug.LogError(name + "has no cost text, add some!");
+        }
+
+        else
+        {
+            costText.text = defenderPrefab.GetStarCost().ToString();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+
+        //on click, loop through and grey all of the buttons out before lighting up the clicked button
+        foreach (DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        FindObjectOfType<DefenderSpawner>().SetSelectedDefender(defenderPrefab);
+    }
+}
